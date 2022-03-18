c44337fb95be52da52303e8f41dc08f3b124b612
Giacomo Rizzi <giacomo.rizzi@onpage.it>
2022-03-18 12:38:01 +0100

    #fix 1.0.52

--- woocommerce-onpage.php
+++ woocommerce-onpage.php
@@ -3,7 +3,7 @@
  * Plugin Name: OnPage for WooCommerce
  * Plugin URI: https://onpage.it/
  * Description: Import your products from Onpage
- * Version: 1.0.51
+ * Version: 1.0.52
  * Author: OnPage
  * Author URI: https://onpage.it
  * Text Domain: onpage
