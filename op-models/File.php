0327509fe4fd1000aa592672be116e4f4030bd3a
Giacomo Rizzi <giacomo.rizzi@onpage.it>
2022-05-06 13:55:20 +0200

    #feat add cdn support

--- op-models/File.php
+++ op-models/File.php
@@ -10,8 +10,23 @@ class File {
     $this->token = $file->token;
   }
 
+  /** @return string|null the image url */
   public function link() {
     return op_file_url($this);
+  }
+
+  /** @return string|null the image url */
+  public function cdn(string $cdn_name = null) {
+    if (!isset($this->cdn)) return null;
+
+    // Get first cdn available
+    if (!$cdn_name) $cdn_name = array_keys($this->cdn)[0];
+
+    // Verify image is present
+    if (!isset($this->cdn[$cdn_name])) return null;
+
+    // Return the cdn url
+    return $this->cdn[$cdn_name];
   }
   
   public function thumb($w = null, $h = null, $contain = false) {
