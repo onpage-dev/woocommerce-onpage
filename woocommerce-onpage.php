78926df4ae07130244e557b51124fc7bb1314cc4
Giacomo Rizzi <gufoes@gmail.com>
2023-02-01 13:46:58 +0100

    1.1.60

--- woocommerce-onpage.php
+++ woocommerce-onpage.php
@@ -4,7 +4,7 @@
  * Plugin Name: OnPage for WooCommerce
  * Plugin URI: https://onpage.it/
  * Description: Import your products from Onpage
- * Version: 1.1.59
+ * Version: 1.1.60
  * Author: OnPage
  * Author URI: https://onpage.it
  * Text Domain: onpage
@@ -173,79 +173,82 @@ add_action('product_cat_edit_form_fields', function ($tag) {
 
 
 // Add the "On Page" column to the woocommerce product list
-add_filter( 'manage_edit-product_columns', function ( $columns ){
+add_filter('manage_edit-product_columns', function ($columns) {
   $columns['onpage_info'] = 'On Page';
   return $columns;
-} );
-add_filter( 'manage_product_posts_custom_column', function ( $column, $product_id ){
-   if ( $column == 'onpage_info' ) {
-     echo '#'.OpLib\PostMeta::where('post_id', $product_id )->where('meta_key', 'op_id*')->pluck('meta_value')->first();
-     // echo $product->get_catalog_visibility();
-     echo "<br>";
-     $res_id = OpLib\PostMeta::where('post_id', $product_id )->where('meta_key', 'op_res*')->pluck('meta_value')->first();
-     if ($res_id) {
-       $res = op_schema()->id_to_res[$res_id] ?? null;
-       if ($res) {
-         echo op_label($res);
-       }
-     }
-     // echo $product->get_catalog_visibility();
-   }
-}, 10, 2 );
+});
+add_filter('manage_product_posts_custom_column', function ($column, $product_id) {
+  if ($column == 'onpage_info') {
+    echo '#' . OpLib\PostMeta::where('post_id', $product_id)->where('meta_key', 'op_id*')->pluck('meta_value')->first();
+    // echo $product->get_catalog_visibility();
+    echo "<br>";
+    $res_id = OpLib\PostMeta::where('post_id', $product_id)->where('meta_key', 'op_res*')->pluck('meta_value')->first();
+    if ($res_id) {
+      $res = op_schema()->id_to_res[$res_id] ?? null;
+      if ($res) {
+        echo op_label($res);
+      }
+    }
+    // echo $product->get_catalog_visibility();
+  }
+}, 10, 2);
 
 
 // Add the "On Page" column to the woocommerce category list
-add_filter( 'manage_edit-product_cat_columns', function ( $columns ){
+add_filter('manage_edit-product_cat_columns', function ($columns) {
   $columns['onpage_info'] = 'On Page';
   return $columns;
-} );
-add_filter( 'manage_product_cat_custom_column', function ( $dep, $column, $product_id ){
-   if ( $column == 'onpage_info' ) {
-       echo '#'.OpLib\TermMeta::where('term_id', $product_id )->where('meta_key', 'op_id*')->pluck('meta_value')->first();
-     // echo $product->get_catalog_visibility();
-     echo "<br>";
-     $res_id = OpLib\TermMeta::where('term_id', $product_id )->where('meta_key', 'op_res*')->pluck('meta_value')->first();
-     if ($res_id) {
-       $res = op_schema()->id_to_res[$res_id] ?? null;
-       if ($res) {
-         echo op_label($res);
-       }
-     }
-     // echo $product->get_catalog_visibility();
-   }
-}, 10, 3 );
+});
+add_filter('manage_product_cat_custom_column', function ($dep, $column, $product_id) {
+  if ($column == 'onpage_info') {
+    echo '#' . OpLib\TermMeta::where('term_id', $product_id)->where('meta_key', 'op_id*')->pluck('meta_value')->first();
+    // echo $product->get_catalog_visibility();
+    echo "<br>";
+    $res_id = OpLib\TermMeta::where('term_id', $product_id)->where('meta_key', 'op_res*')->pluck('meta_value')->first();
+    if ($res_id) {
+      $res = op_schema()->id_to_res[$res_id] ?? null;
+      if ($res) {
+        echo op_label($res);
+      }
+    }
+    // echo $product->get_catalog_visibility();
+  }
+}, 10, 3);
 
 
 // This filter corrects the image url for the data imported from On Page
 add_filter('wp_get_attachment_image_src', function ($image, $attachment_id, $size, $icon) {
   static $sizes = null;
-  if (!$sizes) $sizes = wp_get_registered_image_subsizes();
-  if (!isset($sizes[$size])) return $image;
-  $size = $sizes[$size];
+
   // $image[0] = 'http://newimagesrc.com/myimage.jpg';
   if ($image !== false || strpos($attachment_id, '[{') !== 0) {
     return $image;
   }
+
+  if (!$sizes) $sizes = wp_get_registered_image_subsizes();
+
+  $w = null;
+  $h = null;
+  $contain = false;
+  if (is_array($size)) {
+    $w = @$size[0];
+    $h = @$size[1];
+  } elseif (is_string($size)) {
+    if (!isset($sizes[$size])) return $image;
+    $w = @$sizes[$size]['width'] ?: null;
+    $h = @$sizes[$size]['height'] ?: null;
+    $contain = !(@$sizes[$size]['crop'] ?: false);
+  }
+
+  // Round the size to something that On Page API can understand (modulo 10)
+  $steps_px = 10;
+  if ($w && ($w % $steps_px)) $w += $steps_px - ($w % $steps_px);
+  if ($h && ($h % $steps_px)) $h += $steps_px - ($h % $steps_px);
+
   $json = null;
   try {
     $json = json_decode($attachment_id);
     if (!is_array($json)) return $image;
-
-    $w = null;
-    $h = null;
-    $contain = true;
-    if ($size) {
-      $w = $size['width'] ?: null;
-      $h = $size['height'] ?: null;
-      $contain = !($size['crop'] ?: false);
-
-      // Round the size to something that On Page API can understand (modulo 10)
-      $steps_px = 10;
-      if ($w && ($w % $steps_px)) $w += $steps_px - ($w % $steps_px);
-      if ($h && ($h % $steps_px)) $h += $steps_px - ($h % $steps_px);
-    } else {
-      $w = 1000;
-    }
 
     return array_map(function ($file) use ($w, $h, $contain) {
       return is_admin() ? op_file_remote_url($file, $w, $h, $contain)[2] : op_file_url($file, $w, $h, $contain);
