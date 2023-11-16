069cc5c6d35c2bb2e1ffe7bb466fc17cf8284c4b
Giacomo Rizzi <gufoes@gmail.com>
2023-11-16 15:22:38 +0100

    1.1.77

--- woocommerce-onpage.php
+++ woocommerce-onpage.php
@@ -4,7 +4,7 @@
  * Plugin Name: OnPage for WooCommerce
  * Plugin URI: https://onpage.it/
  * Description: Import your products from Onpage
- * Version: 1.1.76
+ * Version: 1.1.77
  * Author: OnPage
  * Author URI: https://onpage.it
  * Text Domain: onpage
